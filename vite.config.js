27bd4076ea2891990a7b97bb5e6a023c2d3320aa
Faeyza-PC <faeyza@mutiarabangsa.sch.id>
2025-12-11 02:11:56 +0700

    feat: setup pwa manifest and icons

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'ICT Student App',
+        short_name: 'ICT App',
+        description: 'Aplikasi Pembelajaran ICT',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
     // Bundle analyzer (only in build mode)
     visualizer({
       open: false,
